07b20eecf87eaf3e69ff162366388b6d79fc511e
Peter Thompson <peterthompson@roamflyfishing.com>
2017-12-29 15:20:55 -0800

    Create custom scope for react controller.  Create react portfolio item show page.

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,3 +1,11 @@
 class Portfolio < ApplicationRecord
   validates :title, :body, :main_image, :thumb_image, presence: true
+
+  # def self.react
+  #   where(subtitle: "React")
+  # end
+
+  scope :ruby_on_rails, -> { where(subtitle: "Ruby on Rails") }
+
+  scope :react, -> { where(subtitle: "React") }
 end
