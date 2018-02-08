97ca9a76676953bb2a60e0fa194dddd942ee7684
Peter Thompson <peterthompson@roamflyfishing.com>
2018-02-07 16:51:47 -0800

    Implemented initial file uploading via carrierwave for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -9,6 +9,9 @@ class Portfolio < ApplicationRecord
   scope :ruby_on_rails, -> { where(subtitle: "Ruby on Rails") }
   scope :react, -> { where(subtitle: "React") }
 
+  mount_uploader :thumb_image, PortfolioUploader
+  mount_uploader :main_image, PortfolioUploader
+
   after_initialize :set_defaults
 
   def set_defaults
